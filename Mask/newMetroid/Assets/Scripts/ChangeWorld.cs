341a6f8ff9f3bb60bcb02e67759c8a9a441cfa71
lzq2333 <2987664204@qq.com>
2021-12-07 16:36:09 +0800

    ai
    ai

--- Mask/newMetroid/Assets/Scripts/ChangeWorld.cs
+++ Mask/newMetroid/Assets/Scripts/ChangeWorld.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ChangeWorld : MonoBehaviour
 {
+    public static bool canchange = false;
     // Start is called before the first frame update
     void Start()
     {
