6d8a74b070535db9b8d7a8fba33531ce8b5efddd
BofuQWQ <xiaoqi.lai@dukekunshan.edu.cn>
2021-12-08 07:00:20 +0800

    1

--- Mask/newMetroid/Assets/Scripts/TalkPerson.cs
+++ Mask/newMetroid/Assets/Scripts/TalkPerson.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class TalkPerson : MonoBehaviour
 {
     public GameObject NPC;
-    
+    //public Dialogue dialogue;
     public void OnTriggerStay2D(Collider2D collider)
     {Debug.Log("1");
        
         if (collider.tag=="Player" && PlayerController.isTalking == true)
         {
-            
+            //FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
             NPC.SetActive(true);
 
         }
         else
-            NPC.SetActive(false);
+           NPC.SetActive(false);
     }
 
 }
