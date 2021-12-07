053860f784263910fd6df57dfc3650ff5a324511
lzq2333 <2987664204@qq.com>
2021-12-07 21:44:42 +0800

    对话

--- Mask/newMetroid/Assets/Scripts/TalkPerson.cs
+++ Mask/newMetroid/Assets/Scripts/TalkPerson.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class TalkPerson : MonoBehaviour
 {
     public GameObject NPC;
-    private float time;
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player") && PlayerController.isTalking == true)
+    
+    public void OnTriggerStay2D(Collider2D collider)
+    {Debug.Log("1");
+       
+        if (collider.tag=="Player" && PlayerController.isTalking == true)
         {
+            
             NPC.SetActive(true);
 
         }
